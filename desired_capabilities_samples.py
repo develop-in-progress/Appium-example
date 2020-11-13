73b4cefde9c5c3063e547153bf9e410c7ed415e8
develop-in-progress <hellfree22@gmail.com>
2020-11-13 17:35:26 +0200

    added search test

--- desired_capabilities_samples.py
+++ desired_capabilities_samples.py
@@ -2,6 +2,8 @@ class DCSamples:
     desired_capabilities_virtual_pixel2 = {
             "platformName": "Android",
             "platformVersion": "11",
-            "deviceName": "Emulator",
-            "app": "D:\\PyCharm\\Appium-example\\app\\Wikipedia+v2.7.50334-r-2020-11-02.apk"
+            "deviceName": "Android Emulator",
+            "appActivity" : "org.wikipedia.main.MainActivity",
+            "app": "D:\\PyCharm\\Appium-example\\app\\Wikipedia+v2.7.50334-r-2020-11-02.apk",
+            'noReset': 'true'
         }
--- test_main.py
+++ test_main.py
@@ -10,11 +10,10 @@ class TestSearchForm:
     def setup(self):
         desired_capabilities = DCSamples.desired_capabilities_virtual_pixel2
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities=desired_capabilities)
-        self.driver.implicitly_wait(5)
 
     def test_search_form(self):
         main_page = MainPage(self.driver)
-        main_page.skip_familiarization()
+        # main_page.skip_familiarization()
         main_page.tap_search()
         search_page = SearchPage(self.driver)
         search_page.input_search('Python')

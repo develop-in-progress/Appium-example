0861edee5658fc5413abbad2425183754faa8999
develop-in-progress <hellfree22@gmail.com>
2020-11-18 15:01:31 +0200

    added saucelab connection

--- desired_capabilities_samples.py
+++ desired_capabilities_samples.py
@@ -50,7 +50,7 @@ class DCSamples:
             "browserName": "",
             'username': os.environ['SAUCE_USERNAME'],
             'accessKey': os.environ['SAUCE_ACCESS_KEY'],
-            "automationName": 'XCUITest'
+            "automationName": "XCUITest"
 
         }
     }

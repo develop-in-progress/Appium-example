36e1b44ea282798050d84725435eb8f76890cb97
develop-in-progress <hellfree22@gmail.com>
2020-11-19 12:41:41 +0200

    added iOS support for search test

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ import pytest
 class TestSearchForm:
     def setup(self):
         remote_url = "https://ondemand.saucelabs.com:443/wd/hub"
-        caps = DCSamples.desired_capabilities_['Android Saucelab']
+        caps = DCSamples.desired_capabilities_['iOS Saucelab']
         # self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities=caps)
         self.driver = webdriver.Remote(command_executor=remote_url, desired_capabilities=caps)
 
